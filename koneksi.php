cea91501723257851795e40888a534ce76f1190d
1998-droid <tiotiotio98@gmail.com>
2022-07-07 13:22:07 +0700

    commit

--- koneksi.php
+++ koneksi.php
@@ -5,15 +5,4 @@ $password = "";
 $database = "jd";
 
 $koneksi = mysqli_connect($server,$username,$password,$database);
-
-$a = mysqli_query($koneksi, "SELECT * FROM tb_user") or die (mysqli_error($koneksi));
-
-
-
-while($b = mysqli_fetch_array($a)){
-   echo $b['user'];
-   echo $b['id_user'];
-}
-
-
 ?>
