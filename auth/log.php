3a65317a07c6e3235101c02c805256add92552ff
1998-droid <tiotiotio98@gmail.com>
2022-07-11 08:13:15 +0700

    commit

---
+++ auth/log.php
@@ -0,0 +1,90 @@
+
+<head>
+<title>Login V15</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="../assets/login/images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/css-hamburgers/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/animsition/css/animsition.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/vendor/daterangepicker/daterangepicker.css">
+
+<link rel="stylesheet" type="text/css" href="../assets/login/css/util.css">
+<link rel="stylesheet" type="text/css" href="../assets/login/css/main.css">
+
+<meta name="robots" content="noindex, follow">
+<script nonce="891ce73c-f463-4a3d-b119-0e59b40c7bc7">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{},a.zarazData.executed=[],a.zaraz={deferred:[]},a.zaraz.q=[],a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","ecommerce","debug"])a.zaraz[e]=a.zaraz._f(e);a.addEventListener("DOMContentLoaded",(()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];for(n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text),a.zarazData.x=Math.random(),a.zarazData.w=a.screen.width,a.zarazData.h=a.screen.height,a.zarazData.j=a.innerHeight,a.zarazData.e=a.innerWidth,a.zarazData.l=a.location.href,a.zarazData.r=e.referrer,a.zarazData.k=a.screen.colorDepth,a.zarazData.n=e.characterSet,a.zarazData.o=(new Date).getTimezoneOffset(),a.zarazData.q=[];a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin",z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData))),t.parentNode.insertBefore(z,t)}))}(w,d,0,"script");})(window,document);</script></head>
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100">
+<div class="wrap-login100">
+<div class="login100-form-title" style="background-image: url(../assets/login/images/bg-01.jpg);">
+<span class="login100-form-title-1">
+Sign In
+</span>
+</div>
+<form action="pro1.php" method="post" class="login100-form validate-form">
+<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+<span class="label-input100">Username</span>
+<input class="input100" type="text" name="username" placeholder="Enter username">
+<span class="focus-input100"></span>
+</div>
+<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+<span class="label-input100">Password</span>
+<input class="input100" type="password" name="password" placeholder="Enter password">
+<span class="focus-input100"></span>
+</div>
+
+<div class="container-login100-form-btn">
+<button class="login100-form-btn">
+Login
+</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
+<script src="../assets/js/scripts.js"></script>
+<script src="../asset/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<script src="asset/login/vendor/animsition/js/animsition.min.js"></script>
+
+<script src="asset/login/vendor/bootstrap/js/popper.js"></script>
+<script src="asset/login/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="asset/login/vendor/select2/select2.min.js"></script>
+
+<script src="asset/login/vendor/daterangepicker/moment.min.js"></script>
+<script src="asset/login/vendor/daterangepicker/daterangepicker.js"></script>
+
+<script src="asset/login/vendor/countdowntime/countdowntime.js"></script>
+
+<script src="asset/login/js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"716f0ec7eeb32f0a","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
